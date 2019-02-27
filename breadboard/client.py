caf3edde6ea17ffe1ce75596bb6d646327d777c2
Biswaroop Mukherjee <roop@mit.edu>
2019-02-27 16:05:03 -0500

    Using Mixins for image queries

--- breadboard/client.py
+++ breadboard/client.py
@@ -1,13 +1,16 @@
 import requests
 import json
-import pandas as pd
 
 from breadboard.auth import BreadboardAuth
+from breadboard.mixins import ImageMixins
 
 
-class BreadboardClient:
+class BreadboardClient(ImageMixins.ImageMixin):
     def __init__(self, config_path, lab_name=None):
-        # handle no config_path
+
+        if not config_path:
+            raise ValueError("Please enter a directory for your API configuration json file")
+
         with open(config_path) as file:
             api_config = json.load(file)
 
@@ -28,96 +31,10 @@ class BreadboardClient:
 
         self.session = requests.Session()
 
-    def update_image(self, id, image_name, params ):
-        # return all the API data corresponding to a set of images as JSON
-        # todo: validate inputs
-        payload = {
-            'name': image_name
-        }
-        payload = {**payload, **params}
-        if isinstance(id, float):
-            id = int(id)
-        update_url =  '/images/'+str(id)+'/'
-        response = self._send_message('PUT', update_url,
-                            data=json.dumps(payload)
-                            )
-
-        return response
-
-
-
-
-    def get_images_json(self, image_names):
-        # return all the API data corresponding to a set of images as JSON
-        # todo: validate inputs
-        if isinstance(image_names,str):
-            image_names = [image_names]
-
-        namelist = ','.join(image_names)
-        payload = {
-            'lab': self.lab_name,
-            'names': namelist,
-        }
-        response = self._send_message('get', '/images', params=payload)
-        return response
-
-
-
-
-    def get_images_df(self, image_names, paramsin="*"):
-        # Return a pandas dataframe
-        if isinstance(image_names,str):
-            image_names = [image_names]
-        # Get data
-        response = self.get_images_json(image_names)
-        jsonresponse = response.json()
-
-        # Prepare df
-        df = pd.DataFrame(columns = ['imagename'])
-        df['imagename'] = image_names
-
-        # Prepare params:
-        paramsall = set(jsonresponse[0].keys())
-        if paramsin=='*':
-            #  Get all params
-            for jr in jsonresponse:
-                params = set(jr['run']['parameters'].keys())
-                paramsall = paramsall.union(params)
-        else:
-            if isinstance(paramsin, str):
-                paramsin = [paramsin]
-            # use set of params provided
-            paramsall = paramsall.union(set(paramsin))
-
-        removeparams = set([
-                    'run',
-                    'name',
-                    'thumbnail',
-                    'atomsperpixel',
-                    'odpath',
-                    'settings',
-                    'ListBoundVariables',
-                    'camera',
-                    ])
-        paramsall = paramsall - removeparams
-
-        # Populate dataframe
-        for i,r in df.iterrows():
-            for param in paramsall:
-                try: # to get the run parameters
-                    df.at[i,param] = jsonresponse[i]['run']['parameters'][param]
-                except:
-                    try:# to get the bare image parameters
-                        df.at[i,param] = jsonresponse[i][param]
-                    except: # nan the rest
-                        df.at[i,param] = float('nan')
-
-        return df
-
-
-
 
     def _send_message(self, method, endpoint, params=None, data=None):
+        """ Send an HTTP message to the API
+        """
         url = self.api_url + endpoint
         r = self.session.request(method, url, params=params, data=data,
                                  headers=self.auth.headers, timeout=30)
